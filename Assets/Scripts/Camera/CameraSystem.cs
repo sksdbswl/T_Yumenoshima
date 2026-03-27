f48d0d33cd377ea466dda2bc87ee3facbbf7f76f
Voyager_Noh_Yunji <nozee@voyagergames.gg>
2026-03-27 11:03:23 +0900

    freecam 전환 inputsystem 추가

--- Assets/Scripts/Camera/CameraSystem.cs
+++ Assets/Scripts/Camera/CameraSystem.cs
@@ -3,34 +3,23 @@ using UnityEngine;
 
 public class CameraSystem : SingletonBase<CameraSystem>
 {
-    [SerializeField]private CinemachineCamera tpsCamera;
-    [SerializeField]private CinemachineCamera fpsCamera;
-    public CinemachineCamera useCamera;
-    //public CinemachineCamera OppositeCamera => useCamera == tpsCamera ? fpsCamera : tpsCamera;
-    
+    [SerializeField] private PlayerInputHandler inputHandler;
+    [SerializeField] private CinemachineCamera tpsCamera;
+    [SerializeField] private CinemachineCamera freeCamera;
+
+    [HideInInspector] public bool isFreeLook = false;
+
     public override void Init()
     {
         base.Init();
-        useCamera = tpsCamera;
-        //Debug.Log("CameraSystem Initialized");
+        inputHandler = GetComponent<PlayerInputHandler>();
+        inputHandler.OnFreeLook += ToggleFreeLook;
     }
-    
-    public void SetActiveCam()
+
+    public void ToggleFreeLook()
     {
-        var targetCam = useCamera;
-
-        if (targetCam == tpsCamera)
-        {
-            targetCam = fpsCamera;
-        }
-        else
-        {
-            targetCam = tpsCamera;
-        }
-
-        tpsCamera.Priority.Value = (targetCam == tpsCamera) ? 20 : 10;
-        fpsCamera.Priority.Value = (targetCam == fpsCamera) ? 20 : 10;
-    
-        useCamera = targetCam;
+        isFreeLook = !isFreeLook;
+        tpsCamera.gameObject.SetActive(!isFreeLook);
+        freeCamera.gameObject.SetActive(isFreeLook);
     }
 }
